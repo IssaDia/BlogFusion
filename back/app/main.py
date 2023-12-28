806f7cfca408cad8286f1a7ac016593ce530dd4f
IssaDia <44264590+IssaDia@users.noreply.github.com>
2023-12-28 23:30:25 +0100

    changed flask param

--- back/app/main.py
+++ back/app/main.py
@@ -46,4 +46,5 @@ def get_db_articles():
     return jsonify(articles_list)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+   port = int(os.environ.get('PORT', 5000))
+   app.run(host='0.0.0.0', port=port)
